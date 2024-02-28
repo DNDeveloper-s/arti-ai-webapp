2e331a721db662f1ca31bd76c3d635fe5c8d32bd
ronakpustack <ronak@pustack.com>
2024-02-28 12:11:45 +0530

    Updated signin

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -41,11 +41,14 @@ export const authOptions: AuthOptions = {
 
 		// update npm i command
 		FacebookProvider({
-			clientId: '645064660474863',
-			clientSecret: 'cdcb46d7a5014b2e9d6b9936c4156c0d',
+			clientId: "645064660474863",
+			clientSecret: "cdcb46d7a5014b2e9d6b9936c4156c0d",
+			// clientId: process.env.NODE_ENV === 'development' ? process.env.FACEBOOK_DEV_CLIENT_ID : process.env.FACEBOOK_PROD_CLIENT_ID,
+			// clientSecret: process.env.NODE_ENV === 'development' ? process.env.FACEBOOK_DEV_CLIENT_SECRET : process.env.FACEBOOK_PROD_CLIENT_SECRET,
 			authorization: "https://www.facebook.com/v11.0/dialog/oauth?scope=ads_management,pages_show_list,pages_read_engagement",
 			// idToken: true,
-			profile(profile, token) {
+			profile(profile: any, token: any) {
+				console.log(`Profile: ${JSON.stringify(profile)} and token: ${JSON.stringify(token)}`);
 				return {
 					id: profile.id,
 					name: profile.name,
@@ -57,10 +60,12 @@ export const authOptions: AuthOptions = {
 				url: "https://graph.facebook.com/me",
 				params: { fields: "id,name,email,picture" },
 				async request({ tokens, client, provider }) {
+					console.log('making user info request')
 					const userInfo = await client.userinfo(tokens.access_token!, {
 						// @ts-expect-error
 						params: provider.userinfo?.params,
 					})
+					console.log(JSON.stringify(userInfo))
 					return userInfo;
 				},
 			},
@@ -91,155 +96,144 @@ export const authOptions: AuthOptions = {
 
 	],
 	callbacks: {
-		async signIn({ user, account, profile, email, credentials }) {
-			return true
+		async signIn({ user, account, profile }) {
+			// TODO: REFACTOR THIS CODE
+
+
+			if (account?.provider === 'google' && profile?.name) {
+				// Split the "name" field into "first_name" and "last_name"
+				const [first_name, last_name] = profile.name.split(' ');
+
+				// Use the Prisma client to create or update the user
+				const existingUser = await prisma.user.findUnique({
+					where: { email: user.email },
+				});
+
+				if (existingUser) {
+					// Update the user's first_name and last_name
+
+					const accountObject = {
+						type: account.type as string,
+						id_token: account.id_token,
+						token_type: account.token_type,
+						access_token: account.access_token,
+						scope: account.scope,
+						expires_at: account.expires_at,
+						provider: account.provider,
+						providerAccountId: account.providerAccountId
+					}
+
+					const existingAccount = await prisma.account.findUnique({
+						where: {
+							provider_providerAccountId: {
+								provider: account.provider,
+								providerAccountId: account.providerAccountId
+							}
+						}
+					});
+
+					if (!existingAccount) {
+						await prisma.account.create({
+							data: {
+								provider: account.provider as string,
+								providerAccountId: account.providerAccountId as string,
+								type: account.type as string,
+								user: { connect: { id: existingUser.id as string } },
+								id_token: account.id_token,
+								token_type: account.token_type,
+								access_token: account.access_token,
+								scope: account.scope,
+								expires_at: account.expires_at,
+							},
+						});
+					} else {
+						await prisma.account.update({
+							where: {
+								provider_providerAccountId: {
+									provider: account.provider,
+									providerAccountId: account.providerAccountId
+								}
+							},
+							data: accountObject,
+						});
+					}
+
+					await prisma.user.update({
+						where: { email: user.email },
+						data: {
+							accounts: {
+								connect: {
+									provider_providerAccountId: {
+										provider: account.provider,
+										providerAccountId: account.providerAccountId
+									}
+								}
+							}
+						},
+					});
+				} else {
+					// Create a new user with first_name and last_name
+					const _user = await prisma.user.create({
+						data: {
+							email: user.email,
+							first_name,
+							last_name,
+							// Other user properties
+						},
+					});
+
+					await prisma.account.create({
+						data: {
+							provider: account.provider as string,
+							providerAccountId: account.providerAccountId as string,
+							type: account.type as string,
+							user: { connect: { id: _user.id, } },
+							id_token: account.id_token,
+							token_type: account.token_type,
+							access_token: account.access_token,
+							scope: account.scope,
+							expires_at: account.expires_at,
+						},
+					});
+
+				}
+			} else {
+				console.log('signin with facebook was successful')
+			}
+			return true;
 		},
 		async redirect({ url, baseUrl }) {
 			return baseUrl
 		},
 		async session({ session, user, token }) {
 			return session
+			// if (!session || !session.user || !session.user.email) return session;
+
+			// const existingUser = await prisma.user.findUnique({
+			// 	where: { email: session.user.email },
+			// });
+
+			// if (!existingUser) {
+			// 	return session;
+			// }
+
+			// return {
+			// 	...session,
+			// 	user: {
+			// 		first_name: existingUser.first_name,
+			// 		last_name: existingUser.last_name,
+			// 		email: existingUser.email,
+			// 		id: existingUser.id,
+			// 		token
+			// 	}
+			// }
 		},
 		async jwt({ token, user, account, profile, isNewUser }) {
-			return token
-		},
+			return token;
+
+		}
+		// Other callback functions
 	},
-	// callbacks: {
-	// 	async signIn({ user, account, profile }) {
-	// 		// TODO: REFACTOR THIS CODE
-
-
-	// 		if (account?.provider === 'google' && profile?.name) {
-	// 			// Split the "name" field into "first_name" and "last_name"
-	// 			const [first_name, last_name] = profile.name.split(' ');
-
-	// 			// Use the Prisma client to create or update the user
-	// 			const existingUser = await prisma.user.findUnique({
-	// 				where: { email: user.email },
-	// 			});
-
-	// 			if (existingUser) {
-	// 				// Update the user's first_name and last_name
-
-	// 				const accountObject = {
-	// 					type: account.type as string,
-	// 					id_token: account.id_token,
-	// 					token_type: account.token_type,
-	// 					access_token: account.access_token,
-	// 					scope: account.scope,
-	// 					expires_at: account.expires_at,
-	// 					provider: account.provider,
-	// 					providerAccountId: account.providerAccountId
-	// 				}
-
-	// 				const existingAccount = await prisma.account.findUnique({
-	// 					where: {
-	// 						provider_providerAccountId: {
-	// 							provider: account.provider,
-	// 							providerAccountId: account.providerAccountId
-	// 						}
-	// 					}
-	// 				});
-
-	// 				if (!existingAccount) {
-	// 					await prisma.account.create({
-	// 						data: {
-	// 							provider: account.provider as string,
-	// 							providerAccountId: account.providerAccountId as string,
-	// 							type: account.type as string,
-	// 							user: { connect: { id: existingUser.id as string } },
-	// 							id_token: account.id_token,
-	// 							token_type: account.token_type,
-	// 							access_token: account.access_token,
-	// 							scope: account.scope,
-	// 							expires_at: account.expires_at,
-	// 						},
-	// 					});
-	// 				} else {
-	// 					await prisma.account.update({
-	// 						where: {
-	// 							provider_providerAccountId: {
-	// 								provider: account.provider,
-	// 								providerAccountId: account.providerAccountId
-	// 							}
-	// 						},
-	// 						data: accountObject,
-	// 					});
-	// 				}
-
-	// 				await prisma.user.update({
-	// 					where: { email: user.email },
-	// 					data: {
-	// 						accounts: {
-	// 							connect: {
-	// 								provider_providerAccountId: {
-	// 									provider: account.provider,
-	// 									providerAccountId: account.providerAccountId
-	// 								}
-	// 							}
-	// 						}
-	// 					},
-	// 				});
-	// 			} else {
-	// 				// Create a new user with first_name and last_name
-	// 				const _user = await prisma.user.create({
-	// 					data: {
-	// 						email: user.email,
-	// 						first_name,
-	// 						last_name,
-	// 						// Other user properties
-	// 					},
-	// 				});
-
-	// 				await prisma.account.create({
-	// 					data: {
-	// 						provider: account.provider as string,
-	// 						providerAccountId: account.providerAccountId as string,
-	// 						type: account.type as string,
-	// 						user: { connect: { id: _user.id, } },
-	// 						id_token: account.id_token,
-	// 						token_type: account.token_type,
-	// 						access_token: account.access_token,
-	// 						scope: account.scope,
-	// 						expires_at: account.expires_at,
-	// 					},
-	// 				});
-
-	// 			}
-	// 		}
-	// 		return true;
-	// 	},
-	// 	async redirect({ url, baseUrl }) {
-	// 		return baseUrl
-	// 	},
-	// 	async session({ session, user, token }) {
-	// 		if (!session || !session.user || !session.user.email) return session;
-
-	// 		const existingUser = await prisma.user.findUnique({
-	// 			where: { email: session.user.email },
-	// 		});
-
-	// 		if (!existingUser) {
-	// 			return session;
-	// 		}
-
-	// 		return {
-	// 			...session,
-	// 			user: {
-	// 				first_name: existingUser.first_name,
-	// 				last_name: existingUser.last_name,
-	// 				email: existingUser.email,
-	// 				id: existingUser.id,
-	// 				token
-	// 			}
-	// 		}
-	// 	},
-	// 	async jwt({ token, user, account, profile, isNewUser }) {
-	// 		return token;
-
-	// 	}
-	// 	// Other callback functions
-	// },
 	// pages: {
 	// 	// signIn: '/auth',
 	// 	// error: '/auth', // Error code passed in query string as ?error=
